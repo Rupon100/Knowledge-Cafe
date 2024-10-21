51eead0e0311b5190cb169ad036ed373d9d798f6
Rupon <fardiidlamrupon@gmail.com>
2024-10-21 21:08:40 +0600

    adding all file

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+ 
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-4xl bg-blue-600 text-white p-6 text-center font-bold">Header From Header!</h1>
+        </div>
+    );
+};
+
+export default Header;
