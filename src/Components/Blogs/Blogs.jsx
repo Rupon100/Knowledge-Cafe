5d7c5e2c4f4343bd1ca2df89fa65829834769ecb
Rupon <fardiidlamrupon@gmail.com>
2024-10-21 21:14:50 +0600

    fetch data

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,9 +1,19 @@
- 
+import { useState, useEffect } from "react";
+
 
 const Blog = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blog.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+
     return (
         <div>
-            
+            <h2 className="text-xl">Vlog</h2>
         </div>
     );
 };
